1f0778e1f8eaa524ede7dbf06c58305ff6e16e3d
ShreyasLokhande <shreyaslokhande56@gmail.com>
2021-09-20 19:34:31 +0530

    Authentication using Firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCXcWDH3zs-GCDD2Mo_WNtL_OLDkBnoXIU",
+    authDomain: "signin-90747.firebaseapp.com",
+    projectId: "signin-90747",
+    storageBucket: "signin-90747.appspot.com",
+    messagingSenderId: "601857073970",
+    appId: "1:601857073970:web:6feec758aa9393e372ad2b"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ index.js
@@ -0,0 +1,24 @@
+document.getElementById("loginForm").addEventListener("submit",(event)=>{
+    event.preventDefault()
+})
+
+firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+        location.replace("welcome.html")
+    }
+})
+
+function login(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch((error)=>{
+        alert("Error while Login")
+    })
+}
+
+function signUp(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+}
